fb9afc31052ad0c598eab386a9b556572c5b3c89
Somya <somya0610@gmail.com>
2020-11-30 22:50:09 -0800

    MVC-Assignment 2

--- src/main/java/ImageHoster/controller/HomeController.java
+++ src/main/java/ImageHoster/controller/HomeController.java
@@ -1,14 +1,27 @@
 package ImageHoster.controller;
 
+import ImageHoster.model.Image;
+import ImageHoster.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String getPage() {
+    //Write the code to inject the ImageService bean
+    @Autowired
+    ImageService imageService;
+
+    @RequestMapping("/")
+    public String getAllImages(Model model) {
+        //Call getAllImages() method in ImageService class to get the list of all images
+        //Add the list of images in the model with the key as "images"
+        List<Image> images = imageService.getAllImages();
+        model.addAttribute("images", images);
         return "index";
     }
-
 }
