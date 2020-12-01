abe756954bee3dedd1087ea3d7b8d3df7acf89c4
Somya <somya0610@gmail.com>
2020-11-30 22:42:23 -0800

    Image Hoster Project MVC-Assignment 1

---
+++ src/main/java/ImageHoster/controller/HomeController.java
@@ -0,0 +1,14 @@
+package ImageHoster.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getPage() {
+        return "index";
+    }
+
+}
